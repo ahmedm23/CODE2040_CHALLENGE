77ca215debf327591fc4e312409391df51eaeb5f
ahmedm23 <ahmedm23@uw.edu>
2016-10-06 20:23:19 -0700

    Style Update
    Reduced Redundancy

--- connect.py
+++ connect.py
@@ -9,31 +9,27 @@ import http.client
 
 url  = 'challenge.code2040.org' 
 httpServer = http.client.HTTPConnection(url)
-keys = json.dumps({'token': 'befdeea19c258349d4baebaa94ff93e0',             
+token = 'befdeea19c258349d4baebaa94ff93e0'
+keys = json.dumps({'token': token,             
                    'github': 'https://github.com/ahmedm23/CODE2040_CHALLENGE'})               
 headers = {'Content-type': 'application/json'}
+httpServer = http.client.HTTPConnection(url)
 
-
-def connect():
-    httpServer = http.client.HTTPConnection(url)
-
-    headers = {'Content-type': 'application/json'}
-    httpServer.request('POST', '/api/register', keys, headers) 
-    connectionStatus = httpServer.getresponse()  
-    return connectionStatus
+def connect (path, requestKey):
+    httpServer.request('POST', path, requestKey, headers) 
+    return httpServer.getresponse()  
     
-def getString(path, token):
-    httpServer.request('POST', path, json.dumps({'token':token}), headers) 
-    conn = httpServer.getresponse()
+def getString(path, requestKey): 
+    conn = connect(path, requestKey)   
     string = conn.read().decode('utf-8')   
-    print(string)    
-    return string
+    return string   
     
-    
-def sendString(path, token, string)     :
-    httpServer.request('POST', path, json.dumps({'token':token, 'string' : string}), headers) 
-    
-    
-connect    
-string = getString('/api/reverse', 'befdeea19c258349d4baebaa94ff93e0')
-sendString('/api/reverse/validate', 'befdeea19c258349d4baebaa94ff93e0', string[::-1])
+def getNeedle(path, requestKey):
+    conn = connect(path, requestKey) 
+    dict = conn.read().decode('utf-8')
+    print (dict)
+
+connect('/api/register', keys)   
+string = getString('/api/reverse', json.dumps({'token':token}))
+connect('/api/reverse/validate', json.dumps({'token':token, 'string' : string[::-1]}))
+getNeedle('/api/haystack', token)
